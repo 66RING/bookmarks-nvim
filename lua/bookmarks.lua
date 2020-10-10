e16307b2da3a41af2e005c89f1e66444b63f8ebc
66RING <1552087953@qq.com>
2020-10-10 21:15:35 +0800

    more lua

--- lua/bookmarks.lua
+++ lua/bookmarks.lua
@@ -1,26 +1,22 @@
-local utils = require("utils")
 local M = {
-  is_open = false
+  is_open = false,
+  buf_bookmarks = 0
 }
 
-function M.show_marks(title, regs)
-  if M.is_open then
-    return
-  end
+function open()
   local has_set_win, mark_window = pcall(vim.api.nvim_get_var,"bookmarks_window")
 
   if has_set_win==false then
     mark_window = 'vertical botright 30new'
   end
 
-  local format_str = "%5s  %5s  %s"
   marks = vim.api.nvim_exec("marks", true)
-  format = M.get_marks(format_str)
+  local format_str = "%5s  %5s  %s"
+  format = get_marks(format_str)
 
   -- render window
   vim.api.nvim_exec(mark_window, false)
   vim.api.nvim_exec("setlocal filetype=bookmarks nonumber norelativenumber buftype=nofile bufhidden=wipe nobuflisted noswapfile nowrap modifiable statusline=Marks nocursorline nofoldenable", false)
-
   M.buf_bookmarks = vim.api.nvim_get_current_buf()
   M.is_open = true
 
@@ -30,7 +26,7 @@ function M.show_marks(title, regs)
   vim.api.nvim_put(format, "l", false, true)
 end
 
-function M.get_marks(format_str)
+function get_marks(format_str)
   marks = vim.api.nvim_exec("marks", true)
   marks = marks.."\n"
   local t = {}
@@ -44,10 +40,42 @@ function M.get_marks(format_str)
   return t
 end
 
-function M.close()
+function close()
   vim.api.nvim_exec("silent! execute 'bd '"..M.buf_bookmarks, false)
   M.buf_bookmarks = 0
   M.is_open = false
+end
+
+function M.toggle()
+  if M.is_open then
+    return
+  end
+  open()
+  vim.api.nvim_exec("redraw", false)
+  local zoom = false
+
+  local ch
+  local reg
+  while 1 do
+    ch = vim.fn.getchar()
+    reg = vim.fn.nr2char(ch)
+    if reg ~= ' ' then
+        break
+    end
+    if zoom then
+        vim.api.nvim_exec("tab close", false)
+        vim.api.nvim_exec("redraw", false)
+    else
+        vim.api.nvim_exec("tab split", false)
+        vim.api.nvim_exec("redraw", false)
+    end
+    zoom = not zoom
+  end
+
+  close()
+  if ch ~= 27 then
+    vim.api.nvim_exec("'"..reg, false)
+  end
 end
 
 return M

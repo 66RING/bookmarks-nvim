ce9e988ac2137d381b560924d0999892f9d003cc
66RING <1552087953@qq.com>
2020-10-11 22:10:28 +0800

    add some prompt

--- lua/bookmarks.lua
+++ lua/bookmarks.lua
@@ -10,13 +10,12 @@ function open()
     mark_window = 'vertical botright 30new'
   end
 
-  marks = vim.api.nvim_exec("marks", true)
   local format_str = "%5s  %5s  %s"
   format = get_marks(format_str)
 
   -- render window
-  vim.api.nvim_exec(mark_window, false)
-  vim.api.nvim_exec("setlocal filetype=bookmarks nonumber norelativenumber buftype=nofile bufhidden=wipe nobuflisted noswapfile nowrap modifiable statusline=Marks nocursorline nofoldenable", false)
+  vim.api.nvim_command(mark_window)
+  vim.api.nvim_command("setlocal filetype=bookmarks nonumber norelativenumber buftype=nofile bufhidden=wipe nobuflisted noswapfile nowrap modifiable statusline=Marks nocursorline nofoldenable")
   M.buf_bookmarks = vim.api.nvim_get_current_buf()
   M.is_open = true
 
@@ -41,7 +40,7 @@ function get_marks(format_str)
 end
 
 function close()
-  vim.api.nvim_exec("silent! execute 'bd '"..M.buf_bookmarks, false)
+  vim.api.nvim_command("silent! execute 'bd '"..M.buf_bookmarks)
   M.buf_bookmarks = 0
   M.is_open = false
 end
@@ -50,8 +49,11 @@ function M.toggle()
   if M.is_open then
     return
   end
+  vim.api.nvim_command("echohl WarningMsg")
+  vim.api.nvim_command("echo ''")
+  vim.api.nvim_command("echo 'Jump to Mark:'")
   open()
-  vim.api.nvim_exec("redraw", false)
+  vim.api.nvim_command("redraw")
   local zoom = false
 
   local ch
@@ -63,19 +65,23 @@ function M.toggle()
         break
     end
     if zoom then
-        vim.api.nvim_exec("tab close", false)
-        vim.api.nvim_exec("redraw", false)
+        vim.api.nvim_command("tab close")
+        vim.api.nvim_command("redraw")
     else
-        vim.api.nvim_exec("tab split", false)
-        vim.api.nvim_exec("redraw", false)
+        vim.api.nvim_command("tab split")
+        vim.api.nvim_command("redraw")
     end
     zoom = not zoom
   end
 
   close()
   if ch ~= 27 then
-    vim.api.nvim_exec("'"..reg, false)
+    vim.api.nvim_command("'"..reg)
+  else
+    vim.api.nvim_command("echo ''")
+    vim.api.nvim_command("echo 'Jump Cancelled'")
   end
+  vim.api.nvim_command("echohl None")
 end
 
 return M
